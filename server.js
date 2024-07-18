d9a1a501cf9b7e8e929159c9b4017872f3c6cf16
gopileau <vanessagonzalez99@hotmail.com>
2024-07-18 14:19:26 +0200

    Initial commit

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const dotenv = require('dotenv');
-const config = require('config');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
 const catwayRoutes = require('./routes/catways');
 const reservationRoutes = require('./routes/reservations');
 const connectDB = require('./config/db');
 const swaggerSetup = require('./swagger');
-
-dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -31,7 +29,24 @@ app.use(express.static('public'));
 // Swagger documentation
 swaggerSetup(app);
 
-module.exports = app;  
+// Start server
+async function startServer() {
+  try {
+    await connectDB(); // Se connecte à MongoDB en utilisant la configuration définie dans config/db.js
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err.message);
+    process.exit(1);
+  }
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
+
+startServer();
+
+module.exports = app;
 
 
 
