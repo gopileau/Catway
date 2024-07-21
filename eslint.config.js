39e9c56a2cdfd5811fd173b839ed48ce529ebe7c
gopileau <vanessagonzalez99@hotmail.com>
2024-07-21 22:33:46 +0200

    Fix ESLint configuration and update Node.js version

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+// eslint.config.js
+/** @type {import('eslint').Linter.Config} */
+const config = {
+    extends: ['eslint:recommended'],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    rules: {
+      // Ajoutez vos règles personnalisées ici
+    },
+  };
+  
+  module.exports = config;
