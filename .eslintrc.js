34edb3524755765a18964fda9b195322f1c9198b
gopileau <vanessagonzalez99@hotmail.com>
2024-07-21 19:49:27 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+// .eslintrc.js
+module.exports = {
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended'
+    ],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module'
+    },
+    rules: {
+      // vos règles ESLint personnalisées
+    }
+  };
+
+  
